c762939f6213f3904cd243d6f683a03fc5a648e9
Rui Figueiredo <ruimgf95@gmail.com>
2017-10-18 01:06:18 +0100

    add tests

---
+++ p1/src/test.py
@@ -0,0 +1,28 @@
+import unittest
+from solver import main
+
+class TestSolver(unittest.TestCase):
+
+    def t(self,testname):
+        output = str(main([1, "-u", "../testFiles/"+testname+".txt"]))
+        cost = float(output.split("\n")[-1])
+        with open("../testFiles/"+testname+".out", 'r')  as f:
+            expected = f.readlines()
+            expected_cost = float(expected[-1])
+        self.assertEqual(cost, expected_cost)
+
+    def test_uniformed_simple1(self):
+        "simple1.txt [uniformedSearch]"
+        self.t("simple1")
+
+    def test_uniformed_trivial1(self):
+        "trivial1.txt [uniformedSearch]"
+        self.t("trivial1")
+
+    def test_uniformed_trivial2(self):
+        "trivial2.txt [uniformedSearch]"
+        self.t("trivial2")
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=3)
