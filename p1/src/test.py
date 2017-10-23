d6f18f0be14c2d3633eca642a5859bff77d7b035
Rui Figueiredo <ruimgf95@gmail.com>
2017-10-23 17:09:15 +0100

    add informed testes

--- p1/src/test.py
+++ p1/src/test.py
@@ -3,8 +3,8 @@ from solver import main
 
 class TestSolver(unittest.TestCase):
 
-    def t(self,testname):
-        output = str(main([1, "-u", "../testFiles/"+testname+".txt"]))
+    def t(self,testname,type):
+        output = str(main([1, type, "../testFiles/"+testname+".txt"]))
         cost = float(output.split("\n")[-1])
         with open("../testFiles/"+testname+".out", 'r')  as f:
             expected = f.readlines()
@@ -13,20 +13,37 @@ class TestSolver(unittest.TestCase):
 
     def test_uniformed_simple1(self):
         "simple1.txt [uniformedSearch]"
-        self.t("simple1")
+        self.t("simple1","-u")
 
     def test_uniformed_trivial1(self):
         "trivial1.txt [uniformedSearch]"
-        self.t("trivial1")
+        self.t("trivial1","-u")
 
     def test_uniformed_trivial2(self):
         "trivial2.txt [uniformedSearch]"
-        self.t("trivial2")
+        self.t("trivial2","-u")
 
     #@unittest.skip("skipping mir")
     def test_uniformed_mir(self):
         "mir.txt [uniformedSearch]"
-        self.t("mir")
+        self.t("mir","-u")
+
+    def test_informed_simple1(self):
+        "simple1.txt [informedSearch]"
+        self.t("simple1","-i")
+
+    def test_informed_trivial1(self):
+        "trivial1.txt [informedSearch]"
+        self.t("trivial1","-u")
+
+    def test_informed_trivial2(self):
+        "trivial2.txt [informedSearch]"
+        self.t("trivial2","-i")
+
+    #@unittest.skip("skipping mir")
+    def test_informed_mir(self):
+        "mir.txt [informedSearch]"
+        self.t("mir","-i")
 
 
 if __name__ == '__main__':
