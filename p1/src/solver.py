60614ffae65eeb149a34ad216d3cb82dd2731729
candeiasalexandre <candeiasalexandre@gmail.com>
2017-10-24 00:03:26 +0100

    nothing new

--- p1/src/solver.py
+++ p1/src/solver.py
@@ -12,7 +12,7 @@ def main(args):
     if args[1] == '-u':
         p = Problem(g)
     elif args[1] == '-i':
-        p = Problem(g, heur_cena)
+        p = Problem(g, heur_cost_per_kg)
 
     q = PriorityQueue()
     f = GeneralSearch(p, q)
