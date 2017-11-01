c9cecb4be6e43feb87a3375f82f7a64230143de4
Rui Figueiredo <ruimgf95@gmail.com>
2017-11-01 16:00:14 +0000

    mir lp working

--- p1/src/lp.py
+++ p1/src/lp.py
@@ -7,6 +7,9 @@ P = 138.2
 
 prob = LpProblem("test1", LpMinimize)
 
+
+
+
 p1 = LpVariable('p1', lowBound=0, cat='Continuous')
 p2 = LpVariable('p2', lowBound=0, cat='Continuous')
 p3 = LpVariable('p3', lowBound=0, cat='Continuous')
@@ -33,15 +36,15 @@ y10 = LpVariable('y10',cat='Binary')
 prob += y1*cf[0] + cv[0]*p1 + y2*cf[1] + cv[1]*p2  + y3*cf[2] + cv[2]*p3  + y4*cf[3] + cv[3]*p4  + y5*cf[4] + cv[4]*p5 + y6 * cf[5] + cv[5] * p6  + y7*cf[6] + cv[6]*p7  + y8*cf[7] + cv[7]*p8  + y9*cf[8] + cv[8]*p9 + y10*cf[9] + cv[9]*10
 
 prob += p1 <= max[0] * y1
-prob += p2 <= max[0] * y2
-prob += p3 <= max[0] * y3
-prob += p4 <= max[0] * y4
-prob += p5 <= max[0] * y5
-prob += p6 <= max[0] * y6
-prob += p7 <= max[0] * y7
-prob += p8 <= max[0] * y8
-prob += p9 <= max[0] * y9
-prob += p10 <= max[0] * y10
+prob += p2 <= max[1] * y2
+prob += p3 <= max[2] * y3
+prob += p4 <= max[3] * y4
+prob += p5 <= max[4] * y5
+prob += p6 <= max[5] * y6
+prob += p7 <= max[6] * y7
+prob += p8 <= max[7] * y8
+prob += p9 <= max[8] * y9
+prob += p10 <= max[9] * y10
 
 prob += p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10 == P
 
@@ -50,3 +53,5 @@ print(LpStatus[prob.status])
 
 for variable in prob.variables():
     print("{} = {}".format(variable.name, variable.varValue))
+
+print(value(prob.objective))
