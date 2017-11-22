7e5ae41c8013d216937e3eb3e9d4453365b1d9d5
Rui Figueiredo <ruimgf95@gmail.com>
2017-11-22 23:59:32 +0000

    coment code

--- p2/src/iofiles.py
+++ p2/src/iofiles.py
@@ -1,12 +1,15 @@
 import sys
 
 class SentencesReader():
+    """
+        Class to read sentences from stdin.
+    """
     @staticmethod
     def readstdin():
-        with sys.stdin as f :
+        with sys.stdin as f : #open stdin as a file
             lines = f.readlines()
             sentences = []
-            for line in lines:
+            for line in lines: # convert each line to a python object
                 line = line.rstrip()
                 sentences.append(eval(line))
         return sentences
