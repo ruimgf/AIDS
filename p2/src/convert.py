2a2854297a4becc2f051e0acc1f1aaa6918fee61
Rui Figueiredo <ruimgf95@gmail.com>
2017-11-24 00:14:56 +0000

    dist working

--- p2/src/convert.py
+++ p2/src/convert.py
@@ -1,8 +1,11 @@
 import sys
 from iofiles import *
+
+
 def main(args):
     sentences = SentencesReader.readstdin()
     SentencesReader.process_sentences(sentences)
     print(sentences)
+
 if __name__ == '__main__':
     main(sys.argv)
