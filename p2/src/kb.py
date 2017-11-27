e565d25bfdcd3fd7b6ba90b39207b288b396addf
candeiasalexandre <candeiasalexandre@gmail.com>
2017-11-27 14:56:18 +0000

    resolution ok needs tests, and maybe its wrong. Porque este codigo é lixo

--- p2/src/kb.py
+++ p2/src/kb.py
@@ -1,14 +1,15 @@
 class Kb():
     """Class that represents a KB, it may receive a conjunction of disjuntions,
     so it receives a list of lists, every element must be a disjunction"""
-
     ""
-    def __init__(self, sentences_list=none):
+    def __init__(self, sentences_list=None):
         self.sentences = set(sentences_list)
 
-    #apply resolution in the KB, returns TRUE if it is OK, FALSE otherwise
-    def pl_resolution(self):
 
+    def pl_resolution(self):
+    """
+    this function assumes that the list is a list of tupples every tupple defines a disjuntion
+    """
         clauses = list(self.sentences);
         while True:
             new = []
@@ -17,9 +18,35 @@ class Kb():
                     if(x != y):
                         resolvents = pl_resolve(x,y)#apply resolution to the 2 clauses
                         for z in resolvents:
-                            if z == None:#if we get an empty clause, ten i is true
+                            if not(z):#if we get an empty clause, this means that the tupple is empty
                                 return True
-                        new.append(resolvents)
+                            else:
+                                new.append(z)
             if(set(new).issubset(set(clauses))):#check if the new clauses obtained by resolution
                 return False
             clauses.append(new)
+
+    def pl_resolve(x,y):
+        """
+         this function receives two tupples that represent a disjuntion and returns
+         the new clauses that can be deducted by resolution with that pair
+         """
+         resolvents = []
+         for a in x:
+            if(isinstance(a,tupple)):#if it is a tupple is of type ('not',A) so check if A is in y
+                if(a[1] in y):#this means that A is in Y so we can have a
+                    aux_x = list(x)
+                    aux_y = list(y)
+                    aux_x.remove(a)
+                    aux_y.remove(a[1])
+                    aux_x = aux_x + aux_y
+                    resolvents.append(tupple(aux_x))
+            else:
+                if(('not',a) in y):
+                    aux_x = list(x)
+                    aux_y = list(y)
+                    aux_x.remove(a)
+                    aux_y.remove(('not',a))
+                    aux_x = aux_x + aux_y
+                    resolvents.append(tupple(aux_x))
+        return resolvents
