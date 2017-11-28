4572db5a89c33139940106f86659e83d92bf832b
candeiasalexandre <candeiasalexandre@gmail.com>
2017-11-28 18:55:56 +0000

    some errors only in tests

--- p2/src/test.py
+++ p2/src/test.py
@@ -5,13 +5,19 @@ import prove
 class TestSolver(unittest.TestCase):
 
     def prove(self,testname,solution):
-        testfile_path = "../testFiles/" + testname + ".txt"
+        testfile_path = "../testFiles/prove/" + testname + ".txt"
 
         output = prove.main(convert.main(testfile_path).sentences)
         self.assertEqual(solution, output)
 
     def test_1_prove(self):
         self.prove("test1",True)
+
+    def test_2_prove(self):
+        self.prove("test2",False)
+
+    def test_3_prove(self):
+        self.prove("test3",True)
 
 if __name__ == '__main__':
      unittest.main(verbosity=3)
