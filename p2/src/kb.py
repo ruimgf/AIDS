ae69d19db51877e4361f4a7dc0f6fe496daefc11
Rui Figueiredo <ruimgf95@gmail.com>
2017-11-28 15:47:40 +0000

    solver working

--- p2/src/kb.py
+++ p2/src/kb.py
@@ -4,15 +4,17 @@ class Kb():
     so it receives a list of lists, every element must be a disjunction"""
     def __init__(self, sentences_list=None):
         self.sentences = sentences_list
-
+        
 
     def pl_resolution(self):
     #this function assumes that the list is a list of tupples every tupple defines a disjuntion
         clauses = deepcopy(self.sentences)
         new = []
         while True:
-            for x in clauses:
-                for y in clauses:
+            for i in range(len(clauses)):
+                for j in range(i+1,len(clauses)):
+                    x = clauses[i]
+                    y = clauses[j]
                     print('------------------------')
                     print(x)
                     print(y)
@@ -23,7 +25,8 @@ class Kb():
                         if not(z):
                             return True
                         else:
-                            new.append(z)
+                            if z not in new:
+                                new.append(z)
             flag = False
             for element in new:
                 if element not in clauses:
@@ -36,26 +39,24 @@ class Kb():
 
             else: # is a sub set
                 return False
+            print("clauses:" + str(clauses))
+
+def negate_literal(literal):
+    if type(literal) is str:
+        return ('not',literal)
+    else:
+        return literal[1]
 
 def pl_resolve(x,y):
     #this function receives two lists that represent a disjuntion and returns
     #the new clauses that can be deducted by resolution with that pair
     resolvents = []
     for a in x:
-        if isinstance(a,tuple):#implies that this atom is a not A setenence
-            if a[1] in y:
-                aux_x = deepcopy(x)
-                aux_y = deepcopy(y)
-                aux_x.remove(a)
-                aux_y.remove(a[1])
-                aux_x = aux_x.union(aux_y)
-                resolvents.append(aux_x)
-        else:
-            if ('not',a) in y:
-                aux_x = deepcopy(x)
-                aux_y = deepcopy(y)
-                aux_x.remove(a)
-                aux_y.remove(('not',a))
-                aux_x = aux_x.union(aux_y)
-                resolvents.append(aux_x)
+        if negate_literal(a) in y:
+            aux_x = deepcopy(x)
+            aux_y = deepcopy(y)
+            aux_x.remove(a)
+            aux_y.remove(negate_literal(a))
+            aux_x = aux_x.union(aux_y)
+            resolvents.append(aux_x)
     return resolvents
