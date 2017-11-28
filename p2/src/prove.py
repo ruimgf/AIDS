2155dbfe20cf6bafbf6944587dd2e52da6e3704c
candeiasalexandre <candeiasalexandre@gmail.com>
2017-11-28 01:29:54 +0000

    nada

--- p2/src/prove.py
+++ p2/src/prove.py
@@ -1,5 +1,13 @@
 import sys
 def main(args):
-    pass
+    sentences = []
+    with sys.stdin as f : #open stdin as a file
+        for line in lines: # convert each line to a python object
+            line = line.rstrip()
+            sentences.append(tupple(eval(line)))
+
+    print(sentences)
+
+
 if __name__ == '__main__':
     print(main(sys.argv))
