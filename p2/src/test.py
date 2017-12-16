3e94c6f7d63bf154850f19d96a6f8a71e6e46a4c
Rui Figueiredo <ruimgf95@gmail.com>
2017-12-16 23:09:25 +0000

    clean files

--- p2/src/test.py
+++ p2/src/test.py
@@ -5,22 +5,25 @@ import prove
 class TestSolver(unittest.TestCase):
 
     def prove(self,testname,solution):
-        testfile_path = "../testFiles/prove/" + testname + ".txt"
+        testfile_path = "../testFiles/" + testname + ".txt"
 
         output = prove.main(convert.main(testfile_path).sentences)
         self.assertEqual(solution, output)
 
-    def test_1_prove(self):
-        self.prove("test1",True)
+    def test_trival(self):
+        self.prove("trivial",True)
 
-    def test_2_prove(self):
-        self.prove("test2",False)
+    def test_p1(self):
+        self.prove("p1",False)
 
-    def test_3_prove(self):
-        self.prove("test3",True)
+    def test_p2(self):
+        self.prove("p2",False)
 
-    def test_4_prove(self):
-        self.prove("test4",True)
+    def test_p3(self):
+        self.prove("p3",False)
+
+    def test_p4(self):
+        self.prove("p4",True)
 
 if __name__ == '__main__':
      unittest.main(verbosity=3)
